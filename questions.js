554d0c12833d48ac8257a15837f2c6b9c49fff05
Nagy Olivér Emánuel <olinagy2111@gmail.com>
2025-11-30 17:21:38 +0000

    Re-create questions.js with 70 items

---
+++ questions.js
@@ -0,0 +1,643 @@
+questions = [
+    // GAZDASÁGI (Equality vs Markets)
+    // Equality (Baloldal/Állami beavatkozás) vs Markets (Piacpárti/Jobboldal)
+    {
+        "question": "Az államnak hatósági árakkal (árstop) kell beavatkoznia a piacba az infláció letörése érdekében.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "A multinacionális cégeket különadókkal kell sújtani, még ha ez a fogyasztókra is hárul.",
+        "effect": {
+            "econ": 10,
+            "dipl": 5,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Az egykulcsos személyi jövedelemadó (SZJA) igazságosabb, mint a többkulcsos.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "A rezsicsökkentést a jelenlegi formájában minden áron fenn kell tartani.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Az akkumulátorgyárak építése elengedhetetlen a magyar gazdaság jövője szempontjából.",
+        "effect": {
+            "econ": -5,
+            "dipl": -5,
+            "govt": 5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "A pedagógusok és egészségügyi dolgozók bérét azonnal, jelentős mértékben emelni kell, akár más beruházások kárára is.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 5
+        }
+    },
+    {
+        "question": "A magánegészségügy térnyerése jó dolog, mert tehermentesíti az állami ellátást.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Az államnak vissza kellene vásárolnia a stratégiai fontosságú vállalatokat (pl. reptér, energiaszektor).",
+        "effect": {
+            "econ": 10,
+            "dipl": -5,
+            "govt": 5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Az államnak bérlakásprogrammal kellene segítenie a lakhatási válságon.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "A magas fogyasztási adók (ÁFA) helyett inkább a jövedelmeket kellene jobban adóztatni.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "A munkavállalói jogokat (pl. túlóra szabályozása) erősíteni kell a munkaadókkal szemben.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "A családi pótlék összegét az inflációhoz kellene igazítani.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 5
+        }
+    },
+    {
+        "question": "A gazdasági versenyképesség érdekében csökkenteni kell a vállalkozások adminisztrációs terheit.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Az állami támogatások (pl. CSOK) csak felhajtják az ingatlanárakat, ezért meg kellene szüntetni őket.",
+        "effect": {
+            "econ": -5,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Az euró bevezetése elengedhetetlen a magyar gazdaság stabilitásához.",
+        "effect": {
+            "econ": -5,
+            "dipl": 5,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "A magyar mezőgazdaságot protekcionista vámokkal kell védeni az ukrán gabonától.",
+        "effect": {
+            "econ": 10,
+            "dipl": -5,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Az autópályák koncesszióba adása (magánkézbe szervezése) helyes lépés volt.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "A gazdagok és szegények közötti olló túl nagyra nyílt Magyarországon, amit állami eszközökkel kell csökkenteni.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+
+    // DIPLOMÁCIAI (Nation vs Globe)
+    // Nation (Szuverenista/Keleti nyitás) vs Globe (EU-párti/Nyugatos)
+    {
+        "question": "Magyarország helye vitathatatlanul az Európai Unióban és a NATO-ban van.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 5
+        }
+    },
+    {
+        "question": "Brüsszel elhibázott szankciós politikája felelős a gazdasági nehézségekért.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "A 'Keleti Nyitás' politikája (szorosabb kapcsolat Oroszországgal és Kínával) Magyarország érdekét szolgálja.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Ukrajnát fegyverekkel is támogatni kellene az orosz agresszióval szemben.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "A nemzeti szuverenitás védelme fontosabb, mint a nemzetközi szerződések betartása.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "A migrációt minden eszközzel, akár kerítéssel és jogi határzárral is meg kell állítani.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 5,
+            "scty": -5
+        }
+    },
+    {
+        "question": "Támogatnám az Európai Egyesült Államok létrehozását.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "A határon túli magyarok szavazati joga indokolt és megőrzendő.",
+        "effect": {
+            "econ": 0,
+            "dipl": -5,
+            "govt": 0,
+            "scty": -5
+        }
+    },
+    {
+        "question": "A jogállamisági eljárás csak politikai zsarolás Brüsszel részéről.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "A Visegrádi Négyek (V4) együttműködése fontosabb, mint a nyugati szövetségesekkel való kapcsolat.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Svédország és Finnország NATO-csatlakozásának hátráltatása hiba volt.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "A kínai és orosz beruházások (pl. Paks 2, Budapest-Belgrád vasút) nemzetbiztonsági kockázatot jelentenek.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Magyarországnak joga van blokkolni az uniós döntéseket, ha azok sértik a nemzeti érdeket.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "A globális problémákra (pl. klímaváltozás) csak globális összefogással, nemzetállami hatáskörök feladásával lehet megoldást találni.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "A kárpátaljai magyarok jogainak védelme miatt blokkolni kell Ukrajna közeledését az EU-hoz.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Az Európai Ügyészséghez való csatlakozás csökkentené a korrupciót.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Magyarországnak katonailag semlegesnek kellene maradnia a nagyhatalmi konfliktusokban.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "A nyugati kultúra hanyatlása miatt Magyarországnak saját útját kell járnia.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 0,
+            "scty": -5
+        }
+    },
+
+    // POLGÁRI (Liberty vs Authority)
+    // Liberty (Jogállam/Ellenzéki narratíva) vs Authority (Erős vezető/Kormányzati narratíva)
+    {
+        "question": "Magyarországon ma veszélyben van a sajtószabadság és a jogállamiság.",
+        "effect": {
+            "econ": 0,
+            "dipl": 5,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "A Szuverenitásvédelmi Hivatalra szükség van a külföldi befolyásolási kísérletek ellen.",
+        "effect": {
+            "econ": 0,
+            "dipl": -5,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "A civil szervezetek (NGO-k) gyakran külföldi érdekeket szolgálnak, ezért szigorúbban kell őket ellenőrizni.",
+        "effect": {
+            "econ": 0,
+            "dipl": -5,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Erős vezetőre van szükség, aki határozottan irányítja az országot, még ha ez a demokratikus fékek gyengülésével is jár.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": -5
+        }
+    },
+    {
+        "question": "A közmédia kiegyensúlyozott és hiteles tájékoztatást nyújt.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "A korrupció rendszerszintű probléma Magyarországon.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "A sztrájkjog korlátozása (pl. tanárok esetében) elfogadhatatlan.",
+        "effect": {
+            "econ": 5,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "A polgári engedetlenség (pl. tanárok, diákok részéről) legitim eszköz a kormányzati nyomásgyakorlással szemben.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "A Pegasus-ügy (újságírók megfigyelése) kivizsgálása elengedhetetlen.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "A rendőrségnek nagyobb jogköröket kell adni a közbiztonság fenntartása érdekében.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "A bírói függetlenség csorbítása veszélyezteti a demokráciát.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "A választási rendszer (győzteskompenzáció, körzethatárok) a jelenlegi kormánypártnak lejt.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Az önkormányzatok jogköreinek és forrásainak elvonása káros a helyi közösségekre.",
+        "effect": {
+            "econ": 5,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Az iskolák központosított irányítása (KLIK/Tankerületek) javította az oktatás színvonalát.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "A közérdekű adatokhoz való hozzáférést meg kell könnyíteni, a titkosításokat felül kell vizsgálni.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Az állami hirdetések elosztása torzítja a médiapiacot.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Válsághelyzetben (pl. háború, járvány) a kormánynak joga van rendeleti úton kormányozni, a parlament megkerülésével.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+
+    // TÁRSADALMI (Tradition vs Progress)
+    // Tradition (Konzervatív/Keresztény/Anti-LMBTQ) vs Progress (Liberális/Progresszív)
+    {
+        "question": "A család az egy férfi és egy nő házasságán alapul, és az apa férfi, az anya nő.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -5,
+            "scty": -10
+        }
+    },
+    {
+        "question": "A gyermekvédelmi törvény (LMBTQ-tartalmak korlátozása) szükséges a kiskorúak védelmében.",
+        "effect": {
+            "econ": 0,
+            "dipl": -5,
+            "govt": -5,
+            "scty": -10
+        }
+    },
+    {
+        "question": "Az azonos nemű pároknak is joguk kellene legyen az örökbefogadáshoz és a házassághoz.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 5,
+            "scty": 10
+        }
+    },
+    {
+        "question": "Magyarország keresztény kultúrájának megőrzése az állam kiemelt feladata.",
+        "effect": {
+            "econ": 0,
+            "dipl": -5,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "Az egyházi iskolák túlzott támogatást kapnak az állami iskolákhoz képest.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "A 'woke' ideológia és a politikai korrektség veszélyes a társadalomra.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "A könnyűdrogok (pl. marihuána) legalizálása elfogadható lenne.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 10
+        }
+    },
+    {
+        "question": "Az abortuszhoz való jogot szigorítani kellene.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -5,
+            "scty": -10
+        }
+    },
+    {
+        "question": "A nemváltás jogi elismerését lehetővé kellene tenni.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "Az állam és az egyház szétválasztását szigorúbban kellene venni.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "A történelemoktatásnak a nemzeti büszkeségre kell építenie, nem a kritikus szemléletre.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "A klímavédelem érdekében korlátozni kellene a környezetszennyező hagyományokat (pl. tűzijáték).",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "A multikulturalizmus gazdagítja a társadalmat.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "A nők elleni erőszak megfékezésére ratifikálni kellene az Isztambuli Egyezményt.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "A nemzeti szimbólumok (pl. kokárda, zászló) kisajátítása politikai célokra káros.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 5
+        }
+    },
+    {
+        "question": "A tudományos kutatások szabadságát (pl. MTA hálózat) nem korlátozhatja politikai akarat.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 5,
+            "scty": 10
+        }
+    },
+    {
+        "question": "A vidéki életforma és értékek megőrzése fontosabb, mint a városi modernizáció.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -5
+        }
+    }
+];
